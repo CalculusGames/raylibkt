a7355d6bdb1bd46f4df91f4cd4579dccd127f7b1
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-12-06 23:03:42 -0600

    chore: add more utilities (quadruple type, `allocArrayOf` functions)

--- src/common/main/kray/util.kt
+++ src/common/main/kray/util.kt
@@ -3,12 +3,22 @@
 package kray
 
 import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.CArrayPointer
 import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.FloatVar
+import kotlinx.cinterop.IntVar
+import kotlinx.cinterop.IntVarOf
+import kotlinx.cinterop.NativePlacement
+import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.cValue
 import kotlinx.cinterop.get
+import kotlinx.cinterop.nativeHeap
 import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.set
+import kotlinx.cinterop.value
 import raylib.internal.Vector2
 import raylib.internal.Vector3
 
@@ -24,7 +34,7 @@ fun COpaquePointer.toByteArray(size: Int): ByteArray {
 	return ByteArray(size) { i -> ptr[i] }
 }
 
-// pairs & triples
+// pairs, triples and quadruples
 
 /**
  * Converts a pair of integers to a raw Vector2.
@@ -66,7 +76,94 @@ fun Triple<Float, Float, Float>.toVector3(): CValue<Vector3> = cValue<Vector3>{
 
 /**
  * Creates a triplet using the [kotlin.to] paradigm.
+ * @param A type of the first parameter in a triple.
+ * @param B type of the second parameter in a triple.
+ * @param C type of the third parameter in a triple.
  * @param third The third parameter i	n a triple.
  * @return a triple based on the pair with the third item
  */
 infix fun <A, B, C> Pair<A, B>.to(third: C): Triple<A, B, C> = Triple(first, second, third)
+
+/**
+ * A data class that represents a quadruple of values.
+ *
+ * There is no meaning attached to values in this class, it can be used for any purpose.
+ * Quadruple exhibits value semantics, i.e. two quadruples are equal if all four components are equal..
+ *
+ * @param A type of the first value.
+ * @param B type of the second value.
+ * @param C type of the third value.
+ * @param D type of the fourth value.
+ * @property first First value.
+ * @property second Second value.
+ * @property third Third value.
+ * @property fourth Fourth value.
+ */
+data class Quadruple<out A, out B, out C, out D>(
+	val first: A,
+	val second: B,
+	val third: C,
+	val fourth: D
+) {
+	/**
+	 * Returns string representation of the [Quadruple] including its [first], [second], [third] and [fourth] values.
+	 */
+	override fun toString(): String = "($first, $second, $third, $fourth)"
+}
+
+/**
+ * Creates a quadruple using the [kotlin.to] paradigm.
+ * @param A type of the first parameter in a quadruple.
+ * @param B type of the second parameter in a quadruple.
+ * @param C type of the third parameter in a quadruple.
+ * @param D type of the fourth parameter in a quadruple.
+ * @param fourth The fourth parameter in a quadruple.
+ * @return a quadruple based on the triple with the fourth item
+ */
+infix fun <A, B, C, D> Triple<A, B, C>.to(fourth: D): Quadruple<A, B, C, D> =
+	Quadruple(first, second, third, fourth)
+
+// collections
+
+/**
+ * Allocates an array of integers in the given [NativePlacement] and initializes it with the provided [elements].
+ * @return A pointer to the allocated array of integers.
+ */
+fun NativePlacement.allocArrayOf(vararg elements: Int): CArrayPointer<IntVar> {
+	val res = allocArray<IntVar>(elements.size)
+	var index = 0
+	while (index < elements.size) {
+		res[index] = elements[index]
+		++index
+	}
+	return res
+}
+
+/**
+ * Allocates an array of unsigned integers in the given [NativePlacement] and initializes it with the provided [elements].
+ * @return A pointer to the allocated array of integers.
+ */
+fun NativePlacement.allocArrayOf(vararg elements: UInt): CArrayPointer<IntVar> {
+	val res = allocArray<IntVar>(elements.size)
+	var index = 0
+	while (index < elements.size) {
+		res[index] = elements[index].toInt()
+		++index
+	}
+	return res
+}
+
+/**
+ * Converts a collection of integers to a dynamically allocated array.
+ *
+ * **Important:** The caller is responsible for freeing this memory using `nativeHeap.free(array.rawValue)`
+ * when the array is no longer needed to prevent memory leaks.
+ *
+ * @return A pointer to a dynamically allocated array of integers.
+ */
+fun Collection<Int>.toDynamicArray(): CPointer<IntVarOf<Int>> {
+	return nativeHeap.allocArray<IntVarOf<Int>>(size) { i ->
+		value = this@toDynamicArray.elementAt(i)
+	}
+}
+
